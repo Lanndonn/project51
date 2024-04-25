0c98a386fcddf58cddab67f7ceedd16838fc19dd
chad labore <chad.labore@marquette.edu>
2024-04-25 15:07:44 -0500

    Added customers class

---
+++ Customer2.cs
@@ -0,0 +1,26 @@
+﻿namespace project51;
+
+public class Customer2
+{
+    public List<Customer> customerlist { get; set; }
+
+    public Customers()
+    {
+        customerlist = new List<Customer>();
+
+    }
+
+    public Customer Authenticate(string username, string password)
+    {
+        var c = customerlist.Where((o.Username == username) && (o.Password == password));
+
+        if(c.Count > 0)
+        {
+            return c.First();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
