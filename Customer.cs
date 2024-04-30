fc9448a5df9818bd23a8becf895b0668a1df7c8c
chad labore <chad.labore@marquette.edu>
2024-04-30 14:13:58 -0500

    added customer class

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace project51;
+
+public class Customer
+{
+    private static int autoIncrement;
+
+    public int Id{get;}
+    public string username{get;set;}
+    public string password{get;set;}
+    public string firstName{get;set;}
+    public string lastName{get;set;}
+
+    public Customer()
+    {
+        autoIncrement++;
+        Id = autoIncrement;
+        
+    }
+}
