99346a9e43fab1ea231089c886dc381464d10b5f
Landon <landon.traverse@marquette.edu>
2024-04-30 14:16:39 -0500

    added appointment

---
+++ customer.cs
@@ -0,0 +1,20 @@
+﻿using System.Dynamic;
+
+namespace project51;
+
+public class customer
+{
+    private static int autoIncrement;
+
+    public int Id {get;}
+    public string Username {get;set;}
+    public string Password {get;set;}
+    public string FirstName {get;set;}
+    public string LastName {get;set;}
+
+    public Customer()
+    {
+        autoIncrement++;
+        Id = autoIncrement;
+    }
+}
